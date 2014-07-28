d9d0ebd3fd4175789ff2ee62e68830e6f741d39b
preszke <preszke@gmail.com>
2014-07-28 07:09:32 -0700

    Index Lessons with questions texts

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,10 @@ class Lesson < ActiveRecord::Base
   validates :lesson_title, :length => { minimum: 5 }
   accepts_nested_attributes_for :questions, allow_destroy: true
   acts_as_votable
+  def search_data
+    {
+      lesson_title: lesson_title,
+      questions: questions.pluck(:question)
+    }
+  end
 end
