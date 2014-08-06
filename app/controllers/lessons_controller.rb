7c75c591f8afef896ba1434487ef7fe6af528d28
badamziom <badamziom@o2.pl>
2014-08-06 17:53:32 +0200

    Adding test method

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -31,6 +31,11 @@ class LessonsController < ApplicationController
     gon.rabl
   end
 
+  def test
+    @lessons = Lesson.find(params[:id])
+    gon.rabl
+  end
+
   # POST /lessons
   # POST /lessons.json
   def create
