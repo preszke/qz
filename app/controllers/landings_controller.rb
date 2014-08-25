2ccb369ce6da31f71b1ee57750fe088afa65345f
badamziom <badamziom@o2.pl>
2014-08-26 01:34:07 +0200

    Last Added on first page

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,5 +2,6 @@ class LandingsController < ApplicationController
   def index
     #Find all the lessons with word 'dog'
     @lessons = Lesson.search params[:search_title], page: params[:page], per_page: 2
+    @last_lessons = Lesson.last(5).reverse
   end
 end
